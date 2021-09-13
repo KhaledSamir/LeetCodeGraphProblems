720fc020856ef643e81dc4183016dac993019e53
Khaled Saleh <dev.KhaledSaleh@gmail.com>
2021-09-13 00:59:32 -0700

    Added NumberOfComponent problem.

---
+++ LeetcodeProblems/323-Number of Connected Components in an Undirected Graph.cs
@@ -0,0 +1,22 @@
+﻿
+namespace GraphProblems.LeetcodeProblems
+{
+    public class NumberofConnectedComponents
+    {
+        public int CountComponents(int n, int[][] edges)
+        {
+            int count = n;
+            PathCompressionByRank uf = new PathCompressionByRank(n);
+            foreach (var edge in edges)
+            {
+                if (!uf.Connected(edge[0], edge[1]))
+                {
+                    uf.Union(edge[0], edge[1]);
+                    count--;
+                }
+            }
+
+            return count;
+        }
+    }
+}

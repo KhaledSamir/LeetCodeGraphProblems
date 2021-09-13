d3f38c1b46acb9677a977bc9c01a12c3bd73f8f7
Khaled Saleh <dev.KhaledSaleh@gmail.com>
2021-09-13 01:47:32 -0700

    Pushed Graph Valid Tree solution.

---
+++ LeetcodeProblems/261-GraphValidTree.cs
@@ -0,0 +1,24 @@
+﻿
+namespace GraphProblems
+{
+    public class GraphValidTree
+    {
+        public bool ValidTree(int n, int[][] edges)
+        {
+            PathCompressionByRank uf = new PathCompressionByRank(n);
+            int count = n;
+            foreach (var edge in edges)
+            {
+                if (!uf.Connected(edge[0], edge[1]))
+                {
+                    uf.Union(edge[0], edge[1]);
+                    count--;
+                }
+                else
+                    return false;
+            }
+
+            return count == 1;
+        }
+    }
+}

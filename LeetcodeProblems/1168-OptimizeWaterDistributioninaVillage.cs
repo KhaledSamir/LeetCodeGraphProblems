a7ef14ab1fc7a34e7f32fcfacb5417b367f2d5ea
khaled <dev.khaledsaleh@gmail.com>
2021-09-19 21:59:36 -0700

    Added "Water Distribution in a Village" problem.

---
+++ LeetcodeProblems/1168-OptimizeWaterDistributioninaVillage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphProblems.LeetcodeProblems
+{
+    public class OptimizeWaterDistributioninaVillage
+    {
+        public int MinCostToSupplyWater(int n, int[] wells, int[][] pipes)
+        {
+
+            List<int[]> edges = new List<int[]>();
+            int totalCost = 0;
+
+            for (int i = 0; i < wells.Length; i++)
+            {
+                edges.Add(new int[] { 0, i + 1, wells[i] });
+            }
+
+            foreach (var pipe in pipes)
+            {
+                edges.Add(pipe);
+            }
+
+            var orderedEdges = edges.OrderBy(item => item[2]);
+
+            PathCompressionByRank uf = new PathCompressionByRank(n);
+            foreach (var edge in orderedEdges)
+            {
+                if (!uf.Connected(edge[0], edge[1]))
+                {
+                    uf.Union(edge[0], edge[1]);
+                    totalCost += edge[2];
+                }
+            }
+
+            return totalCost;
+        }
+    }
+}
